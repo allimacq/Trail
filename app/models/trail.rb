4d4ba1b5bb90aa91fb8a2390b23682b49b6eca3e
Allison <allimacq@gmail.com>
2021-03-31 12:03:07 -0400

    add slug methods to trail class

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,4 +1,15 @@
 class Trail < ActiveRecord::Base
     belongs_to :user
     belongs_to :state
+
+    def slug
+        self.name.strip.split.join("-")
+    end
+
+    def self.find_by_slug(slug)
+        to_find = slug.split("-").collect do |name|
+            name
+        end.join(" ")
+        self.find_by(name: to_find)
+    end
 end
